47356ba2b58b994eb2ec164775f9c7c04ebf00b5
Javaney <javaneyt@icloud.com>
2023-08-12 14:17:23 -0400

    daily push of syntax changes

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,36 @@
-import React, { UseState } from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
+import { auth } from "./firebase.js";
+
 
 
 
 
 function Login() {
-  const [email, Setemail] = useState('');
+  const [email, setEmail] = useState('');//empty string intead of null
   const [password, setPassword] = useState('');
+
+  const signIn = e => {
+    e.preventDefault()
+
+  } //this is to prevent the automatic refresh of the page when user signs in
+
+  const register = e => {
+    e.preventDefault();
+
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((auth) => {
+      //this means it successfully created a new uer wit email and pasword
+      console.log(auth);
+    })
+    .catch(error => alert(error.message))
+
+  }
+
+
+
   return (
     <div className='login'>
       <Link to='/'>
@@ -20,13 +43,16 @@ function Login() {
         <h1>Sign-In</h1>
 
         <form className='login__form'>
-          <h5>e-mail</h5>
-          <input type='text' />
+          <h5>E-mail</h5>
+          <input type='text' value={email} onChange=
+          {e => setEmail(e.target.value)} />
 
           <h5>Password</h5>
-          <input type='password' />
+          <input type='password' value={password} onChange=
+          {e => setPassword(e.target.value)}/>
 
-          <button className='login__signInButton'>Sign In</button>
+          <button type='submit' onClick={signIn}
+          className='login__signInButton'>Sign In</button>
         </form>
 
         <p> 
@@ -35,7 +61,8 @@ function Login() {
           Notice.
         </p>
 
-        <button className='login__registerButton'>Create your Amazon Account</button>
+        <button onClick={register}
+         className='login__registerButton'>Create your Amazon Account</button>
       </div>
     </div>
   )
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,9 @@
+import { auth, db } from "./firebase";
+import firebase from './firebase';
+//iort firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA8zRGdDsGKxs6O-UZoCcGd0tFxx7Om5CE",
@@ -8,3 +14,11 @@ const firebaseConfig = {
   appId: "1:19225431782:web:6576f5cb0abdb789bd3527",
   measurementId: "G-MX6JQGHZN2"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+//const db = firebaseApp.firestore(); //firestore is the real-time db in fb
+//const auth = firebase.auth(); //gives us a variable to handle sign-ins etc
+
+
+export { db, auth };
