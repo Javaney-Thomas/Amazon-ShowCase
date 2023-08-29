69a685f633cf15cfba64054ff6efb33272d1574a
Javaney <javaneyt@icloud.com>
2023-08-29 14:35:34 -0400

    refresher

--- src/Checkout.js
+++ src/Checkout.js
@@ -8,7 +8,7 @@ import Header from './Header';
 
 
 const Checkout = () => {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className='checkout'>
       <div className='checkout__left'>
@@ -21,6 +21,7 @@ const Checkout = () => {
           alt=""
         />
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className='checkout__title'>
             Your shopping basket
           </h2>
